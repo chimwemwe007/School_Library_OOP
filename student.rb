7775365468e3cd759644f1962d8f3e19e84f92cb
Chimwemwe Mkandawire <chimzymk@gmail.com>
2022-10-04 22:01:18 +0200

    Update student.rb
    Add permissions for name and classroom to initialise constructor

--- student.rb
+++ student.rb
@@ -2,8 +2,9 @@ require_relative 'person'
 
 class Student < Person
   def initialize(classroom)
-    super(name, age, parent_permission)
+    super(name - unknown, age, parent_permission = true)
     @classroom = classroom
+    
   end
 
   def play_hooky
